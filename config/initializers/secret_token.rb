7d19294e2bcdc28ecab79f5a4a49623765ac4416
Andrew Wittrock <awittrock@gmail.com>
2014-04-20 15:25:19 -0500

    initial commit using redditkit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RedditScraper::Application.config.secret_key_base = 'bd83e7d7fea7ab6746403c5d3b37e99364adfd91d1cec41d3d3423da98cac483d01acb0b34d93a8267c975be7248df518456061c7563d3f95deab2bb8d1a2874'
